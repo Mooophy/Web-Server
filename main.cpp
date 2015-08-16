c853d4ae012285d9a3b206e949bf86f8916959aa
yue <MoophyAlan@gmail.com>
2015-08-16 17:54:13 +1200

    add namespace

--- main.cpp
+++ main.cpp
@@ -16,101 +16,99 @@
 #include <condition_variable>
 #include <future>
 
-class ThreadPool {
-public:
-    ThreadPool(size_t);
-    template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args)
-            -> std::future<typename std::result_of<F(Args...)>::type>;
-    ~ThreadPool();
-private:
-    // need to keep track of threads so we can join them
-    std::vector< std::thread > workers;
-    // the task queue
-    std::queue< std::function<void()> > tasks;
+namespace ccur
+{
+    class ThreadPool {
+    public:
+        ThreadPool(size_t);
 
-    // synchronization
-    std::mutex queue_mutex;
-    std::condition_variable condition;
-    bool stop;
-};
+        template<class F, class... Args>
+        auto enqueue(F &&f, Args &&... args)
+                -> std::future<typename std::result_of<F(Args...)>::type>;
+
+        ~ThreadPool();
+
+    private:
+        // need to keep track of threads so we can join them
+        std::vector<std::thread> workers;
+        // the task queue
+        std::queue<std::function<void()> > tasks;
+
+        // synchronization
+        std::mutex queue_mutex;
+        std::condition_variable condition;
+        bool stop;
+    };
 
 // the constructor just launches some amount of workers
-inline ThreadPool::ThreadPool(size_t threads)
-        :   stop(false)
-{
-    for(size_t i = 0;i<threads;++i)
-        workers.emplace_back(
-                [this]
-                {
-                    for(;;)
-                    {
-                        std::function<void()> task;
+    inline ThreadPool::ThreadPool(size_t threads)
+            : stop(false) {
+        for (size_t i = 0; i < threads; ++i)
+            workers.emplace_back(
+                    [this] {
+                        for (; ;) {
+                            std::function<void()> task;
 
-                        {
-                            std::unique_lock<std::mutex> lock(this->queue_mutex);
-                            this->condition.wait(lock,
-                                                 [this]{ return this->stop || !this->tasks.empty(); });
-                            if(this->stop && this->tasks.empty())
-                                return;
-                            task = std::move(this->tasks.front());
-                            this->tasks.pop();
+                            {
+                                std::unique_lock<std::mutex> lock(this->queue_mutex);
+                                this->condition.wait(lock,
+                                                     [this] { return this->stop || !this->tasks.empty(); });
+                                if (this->stop && this->tasks.empty())
+                                    return;
+                                task = std::move(this->tasks.front());
+                                this->tasks.pop();
+                            }
+
+                            task();
                         }
-
-                        task();
                     }
-                }
-        );
-}
+            );
+    }
 
 // add new work item to the pool
-template<class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args)
--> std::future<typename std::result_of<F(Args...)>::type>
-{
-    using return_type = typename std::result_of<F(Args...)>::type;
+    template<class F, class... Args>
+    auto ThreadPool::enqueue(F &&f, Args &&... args)
+    -> std::future<typename std::result_of<F(Args...)>::type> {
+        using return_type = typename std::result_of<F(Args...)>::type;
 
-    auto task = std::make_shared< std::packaged_task<return_type()> >(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-    );
+        auto task = std::make_shared<std::packaged_task<return_type()> >(
+                std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+        );
 
-    std::future<return_type> res = task->get_future();
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
+        std::future<return_type> res = task->get_future();
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
 
-        // don't allow enqueueing after stopping the pool
-        if(stop)
-            throw std::runtime_error("enqueue on stopped ThreadPool");
+            // don't allow enqueueing after stopping the pool
+            if (stop)
+                throw std::runtime_error("enqueue on stopped ThreadPool");
 
-        tasks.emplace([task](){ (*task)(); });
+            tasks.emplace([task]() { (*task)(); });
+        }
+        condition.notify_one();
+        return res;
     }
-    condition.notify_one();
-    return res;
-}
 
 // the destructor joins all threads
-inline ThreadPool::~ThreadPool()
-{
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        stop = true;
+    inline ThreadPool::~ThreadPool() {
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            stop = true;
+        }
+        condition.notify_all();
+        for (std::thread &worker: workers)
+            worker.join();
     }
-    condition.notify_all();
-    for(std::thread &worker: workers)
-        worker.join();
-}
 
-template <typename Socket>
-void send_file(std::string const& filename, Socket const& socket)
-{
-    std::ifstream ifs{ filename };
-    for(std::string str; ifs >> str; )
-    {
-        auto msg = str + "\r\n";
-        send(socket, msg.c_str(), msg.size(), 0);
+    template<typename Socket>
+    void send_file(std::string const &filename, Socket const &socket) {
+        std::ifstream ifs{filename};
+        for (std::string str; ifs >> str;) {
+            auto msg = str + "\r\n";
+            send(socket, msg.c_str(), msg.size(), 0);
+        }
     }
 }
-
 // Define the port number to identify this process
 #define MYPORT 3490
 
@@ -132,7 +130,7 @@ int main()
 
     // Allow up to 10 incoming connections
     const auto limit = 10;
-    ThreadPool pool{ limit };
+    ccur::ThreadPool pool{ limit };
     listen(soc,limit);
     std::cout << "listenning\n";
 
@@ -141,12 +139,12 @@ int main()
         auto request_handler = [&] (int socket){
             char data[512];
             char filename[256];
-            auto n = recv(socket, data, 512, 0);              // recieve the request using fd
-            data[n] = 0;                          // NUL terminate it
+            auto size = recv(socket, data, 512, 0);              // recieve the request using fd
+            data[size] = 0;                          // NUL terminate it
             sscanf(data, "GET /%s ", filename);   // get the name of the file
             send(socket, header.c_str(), header.size(), 0);
             std::this_thread::sleep_for(std::chrono::seconds(10));
-            send_file(filename, socket);
+            ccur::send_file(filename, socket);
             close(socket);                    // close the socket
         };
 
